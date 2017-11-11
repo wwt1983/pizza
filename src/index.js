53c76baf99f48dd437aef21aaa3f0209bc014f73
VALENTINA\valentina <wwt1983@yandex.ru>
2017-11-12 03:13:01 +0500

    upd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import './main.css'
+import  './main.css'
 
-document.write('Hello React/Redux!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import reducers from 'reducers'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Provider} from 'react-redux'
+import {Router, Route} from 'react-router'
+import Layout from 'containers/layout'
+import Pizza from 'containers/pizza'
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component = {Layout}>
+                 <Route path='/' component={Pizza} />
+             </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
