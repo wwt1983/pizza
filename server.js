9fa17edc0bfcf39d6a6932c8c59289dbd436a13f
VALENTINA\valentina <wwt1983@yandex.ru>
2017-11-25 02:48:17 +0500

    send email

--- server.js
+++ server.js
@@ -5,25 +5,52 @@ var config = require('./webpack.config')
 var express = require('express')
 var app = express()
 var cors = require('cors')
+var bodyParser = require('body-parser')
+var nodeMailer = require('nodemailer');
 
 var compiler = webpack(config)
 
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
 app.use(webpackHotMiddleware(compiler))
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}))
 // serve static assets normally
 app.use(express.static(__dirname + '/public'))
-// Handles all routes so you do not get a not found error
+
+var serverFunctions = require('./serverFunctions');
 
 
-app.get('/', function (req, res) {
-    console.log('hello world')
-    response.sendFile(__dirname + '/public/' + 'index.html')
-})
+app.post('/sendemail', (req, res) => {
+    console.log('test send req.body order: '+  serverFunctions.returnJsonObj(req.body.order))
+    if (!req.body.order) {
+        return res.status(400).send({error: true, message: 'Please provide order'});
+    }else {
+        let transporter = nodeMailer.createTransport({ host: 'smtp.gmail.com', port: 465, secure: true, auth:{ user: 'piratridze@gmail.com', pass: 'pirat1983' }});
+        //письмо о заказе нам
+        let mailOptions = serverFunctions.mailerOptions(req.body.order);
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+        });
+        //письмо о заказе клиенту
+        let mailOptionsClient = serverFunctions.mailerOptionsForClient(req.body.order.email);
+        transporter.sendMail(mailOptionsClient, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+        });
+        return res.send({ error: false, data: 'ok', message: 'New order has been send successfully.' });
+    }
+});
+
 app.get('*', function (request, response){
     response.sendFile(__dirname + '/public/' + 'index.html')
-})
+});
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('server express listen PORT ' + port)
-})
+});
---
+++ serverFunctions.js
@@ -0,0 +1,27 @@
+module.exports = {
+    returnJsonObj : (obj) => {
+      return JSON.stringify(obj)
+    },
+    mailerOptions : (obj) => {
+    return {
+        from: '"Pizza Mozzarella" <piratridze@gmail.com', // sender address
+        to: 'wwt1983@yandex.ru', // list of receivers
+        subject: 'Заказ с сайта', // Subject line
+        //text: 'Заказ: тел: ' + obj.phone + '\n имя: ' + obj.name + '\n адресс: ' + obj.address + '\n стоимость: ' + obj.price + '\n позиции ' + obj.info, // plain text body
+        html: '<strong>Заказ </strong>' +
+        '<br/> Тел: ' + obj.phone +
+        '<br/> Имя: ' + obj.name +
+        '<br/> Адрес: ' + obj.address +
+        '<br/> Общая стоимость: заказа: ' + obj.price + ' руб. <br/>' +
+        '<br/> Позиции ' + JSON.stringify(obj.info) + ']'// html body
+    };
+    },
+    mailerOptionsForClient : (email) => {
+        return {
+            from: '"Pizza Mozzarella" <piratridze@gmail.com', // sender address
+            to: email, // list of receivers
+            subject: 'Заказ с сайта пиццерии-траттории Моццарелла', // Subject line
+            html: 'Спасибо, Ваш заказ принят к обработке.<br/><image src="/upload/mozzarella_logo.png" />'
+        };
+    },
+}
