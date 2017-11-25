c031f2277550defe18d060aa456088dbf04e2c57
VALENTINA\valentina <wwt1983@yandex.ru>
2017-11-26 02:23:48 +0500

    upd style

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('*', function (request, response){
 
 
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3001;
 app.listen(port, function () {
     console.log('server express listen PORT ' + port)
 });
