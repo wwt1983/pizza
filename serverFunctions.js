fa68bdd9f4fd3ff26078f1b3b574b1ee78ec101f
VALENTINA\valentina <wwt1983@yandex.ru>
2018-01-09 01:53:28 +0500

    fix send email +

--- serverFunctions.js
+++ serverFunctions.js
@@ -1,27 +1,32 @@
 module.exports = {
-    returnJsonObj : (obj) => {
+    returnJsonObj: (obj) => {
         return JSON.stringify(obj)
     },
-    mailerOptions : (obj) => {
+    mailerOptions: (obj) => {
+        var txt = '';
+        for (let i = 0; i < obj.info.length; i++) {
+            txt += 'Позиция ' + (i + 1) + ': ' + obj.info[i].name + '; число: ' + obj.info[i].count + '<br/>';
+        }
+        
         return {
             from: '"Pizza Mozzarella" <info@mozzarella174.ru', // sender address
             to: 'wwt1983@yandex.ru, dariko2@yandex.ru, sv83a@ya.ru, info@mozzarella174.ru', // list of receivers
             subject: 'Заказ с сайта', // Subject line
             //text: 'Заказ: тел: ' + obj.phone + '\n имя: ' + obj.name + '\n адресс: ' + obj.address + '\n стоимость: ' + obj.price + '\n позиции ' + obj.info, // plain text body
             html: '<strong>Заказ </strong>' +
-            '<br/> Тел: ' + obj.phone +
-            '<br/> Имя: ' + obj.name +
-            '<br/> Адрес: ' + obj.address +
-            '<br/> Общая стоимость: заказа: ' + obj.price + ' руб. <br/>' +
-            '<br/> Позиции ' + JSON.stringify(obj.info) + ']'// html body
+                '<br/> Тел: ' + obj.phone +
+                '<br/> Имя: ' + obj.name +
+                '<br/> Адрес: ' + obj.address +
+                '<br/> Общая стоимость: заказа: ' + obj.price + ' руб. <br/>' +
+                '<br/>  ' + txt // html body
         };
     },
-    mailerOptionsForClient : (email) => {
+    mailerOptionsForClient: (email) => {
         return {
             from: '"Pizza Mozzarella" <info@mozzarella174.ru', // sender address
             to: email, // list of receivers
             subject: 'Заказ с сайта пиццерии-траттории Моццарелла', // Subject line
             html: 'Спасибо, Ваш заказ принят к обработке.<br/><image src="/upload/mozzarella_logo.png" />'
         };
-    },
+    }
 }
