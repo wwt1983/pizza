d3d5116310c5794666e86862fa1e68226b961dcf
VALENTINA\valentina <wwt1983@yandex.ru>
2018-01-18 05:06:05 +0500

    port

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('*', function (request, response) {
 
 
 
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 80;
 app.listen(port, function () {
     console.log('server express listen PORT ' + port)
 });
